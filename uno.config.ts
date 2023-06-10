5b47cd6ce5e4a7824e4fc32ddbc702963857aefa
Hussein Najah <husseinnajah123@gmail.com>
2023-06-10 19:55:12 +0300

    Laravel Project C-1

---
+++ uno.config.ts
@@ -0,0 +1,11 @@
+// uno.config.ts
+import {
+    defineConfig,
+    presetAttributify,
+    presetUno,
+    presetIcons,
+} from "unocss";
+
+export default defineConfig({
+    presets: [presetUno(), presetAttributify(), presetIcons()],
+});
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import UnoCSS from "unocss/vite";
+
+export default defineConfig({
+    plugins: [
+        UnoCSS({
+            extraContent: { filesystem: ["./resources/views/**/*.blade.php"] },
+        }),
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true,
+        }),
+    ],
+});
