48e8eb9c3613ff2259561a47fc014bbc8c4fb807
Hussein Najah <husseinnajah123@gmail.com>
2023-06-12 23:20:40 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('travels', [TravelController::class, 'index']);
 
-Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']); 
+Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
