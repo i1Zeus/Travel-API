f7718ec1ddd652250b1acd40cc53ddce017a8014
Hussein Najah <husseinnajah123@gmail.com>
2023-06-12 21:38:24 +0300

    Update TourController.php

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\TourResource;
-use App\Models\Tour;
 use App\Models\Travel;
-use Illuminate\Http\Request;
 
 class TourController extends Controller
 {
