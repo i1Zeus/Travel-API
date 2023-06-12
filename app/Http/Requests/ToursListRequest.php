32b2f43d6d373669510ff95ecb8b0865f4b3f8d0
Hussein Najah <husseinnajah123@gmail.com>
2023-06-12 23:21:21 +0300

    Update ToursListRequest.php

--- app/Http/Requests/ToursListRequest.php
+++ app/Http/Requests/ToursListRequest.php
@@ -17,11 +17,6 @@ class ToursListRequest extends FormRequest
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, mixed>
-     */
     public function rules(): array
     {
         return  [
@@ -30,21 +25,15 @@ class ToursListRequest extends FormRequest
             'dateFrom' => 'date',
             'dateTo' => 'date',
             'sortBy' => Rule::in(['price_in_cents']),
-            'sortOrder' => Rule::in(['asc', 'desc'])
+            'sortOrder' => Rule::in(['asc', 'desc']),
         ];
     }
-
-    /**
-     * Get the validation messages that apply to the request.
-     *
-     * @return array<string, mixed>
-     */
 
     public function messages(): array
     {
         return [
             'sortBy' => 'The "sortby" accepts only "price" .',
-            'sortOrder' => 'The sort order accepts only "asc" and "desc".'
+            'sortOrder' => 'The sort order accepts only "asc" and "desc".',
         ];
     }
 }
