35ef3b6734ba1689267dcb54e638bd000f049a62
Hussein Najah <husseinnajah123@gmail.com>
2023-06-12 23:20:37 +0300

    Update TourController.php

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -3,17 +3,42 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ToursListRequest;
 use App\Http\Resources\TourResource;
 use App\Models\Travel;
 
 class TourController extends Controller
 {
-    public function index(Travel $travel)
+    public function index(Travel $travel, ToursListRequest $request)
     {
-        $tours= $travel->tours()
-            ->orderBY('starting_date')
+        $tours = $travel->tours()
+            ->when($request->priceFrom, function ($query) use ($request) {
+                $query->where('price_in_cents', '>=', $request->priceFrom * 100);
+            })
+            ->when($request->priceTo, function ($query) use ($request) {
+                $query->where('price_in_cents', '<=', $request->priceTo * 100);
+            })
+            ->when($request->dateFrom, function ($query) use ($request) {
+                $query->where('starting_date', '>=', $request->dateFrom);
+            })
+            ->when($request->dateTo, function ($query) use ($request) {
+                $query->where('starting_date', '<=', $request->dateTo);
+            })
+            ->when($request->sortBy && $request->sortOrder, function ($query) use ($request) {
+                $query->orderBy($request->sortBy, $request->sortOrder);
+            })
+            ->orderBy('starting_date')
             ->paginate();
 
         return TourResource::collection($tours);
     }
+
+    /* 
+    * Notes:
+    * ->when($request->sortBy && $request->sortOrder, function ($query) use ($request) {
+    ?   checks if the sortBy value is in allowed values.
+    !   if (!in_array($request->sortOrder, ['asc', 'desc'])) return;
+    *   $query->orderBy($request->sortBy, $request->sortOrder);
+    * })
+    */
 }
