5a474f8b397626ed95fa657e11382beb6be5f5af
Hussein Najah <husseinnajah123@gmail.com>
2023-06-14 18:02:16 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TravelController;
 use App\Http\Controllers\Api\V1\TourController;
+use App\Http\Controllers\Api\V1\UserController;
+use App\Http\Controllers\Api\V1\Admin;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +22,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//* get all travel
 Route::get('travels', [TravelController::class, 'index']);
 
+//* get all tours for a travel
 Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
+
+Route::prefix('admin')->group(function () {
+    //* create a new travel
+    Route::post('travels', [Admin\TravelController::class, 'store']);
+});
