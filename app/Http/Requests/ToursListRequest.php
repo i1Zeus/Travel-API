7df45c49f4fa6ed17f8be944abb2315b5ab0fbaf
Hussein Najah <husseinnajah123@gmail.com>
2023-06-14 21:43:06 +0300

    Update ToursListRequest.php

--- app/Http/Requests/ToursListRequest.php
+++ app/Http/Requests/ToursListRequest.php
@@ -7,11 +7,6 @@ use Illuminate\Validation\Rule;
 
 class ToursListRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
         return true;
