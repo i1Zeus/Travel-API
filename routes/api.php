410fd1b2e8b9c583e87d5cf57fabad04e1350c72
Hussein Najah <husseinnajah123@gmail.com>
2023-06-15 12:27:19 +0300

    create tour by admin

--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,7 @@ Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
 Route::prefix('admin')->middleware(['auth:sanctum', 'role:admin'])->group(function () {
     //! create a travel
     Route::post('travels', [Admin\TravelController::class, 'store']);
+    Route::post('travels/{travel}/tours', [Admin\TourController::class, 'store']);
 });
 
 //! Login
