cae823025dfbbf895ea13312e7d6887afeac6f4a
Hussein Najah <husseinnajah123@gmail.com>
2023-06-18 23:17:55 +0300

    pint #3

--- app/Http/Controllers/Api/V1/Admin/TourController.php
+++ app/Http/Controllers/Api/V1/Admin/TourController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\TourRequest;
 use App\Http\Resources\TourResource;
 use App\Models\Travel;
-use Illuminate\Http\Request;
 
 class TourController extends Controller
 {
